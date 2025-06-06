a42ff8be9a293b4ebc395d9364391ea28717ff81
aayan8 <348752791@gapps.yrdsb.ca>
2025-06-06 15:00:25 +0000

    stared main menu, addded point value for question, chekd if value was valid

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,70 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
+        String[] categories = new String[100];
+        int[] pointAmount = new int[100];
+        String[] questionText = new String[100];
+        String[] questionAnswer = new String[100];
+        int questionAmmount = 0;
+
+        while (true) {
+            System.out.println("Jeopardy Main Menu");
+            System.out.println("1. Play Game");
+            System.out.println("2. Add Question");
+            System.out.println("3. Exit");
+            System.out.println("Select an option");
+            String menuChoice = sc.nextLine();
+            
+            if (input.equals("1")) {
+                System.out.println("");
+            } else if (input.equals("2")) {
+                System.out.println("Enter category:");
+                categories[questionAmount] = sc.nextLine();
+
+                int points = 0;
+                while (true) {
+                    System.out.println("Enter point value (number): ");
+                    String pointInput = sc.nctLine();
+
+                    boolean validPoint = true;
+                    if (pointInput.lenght() == 0) {
+                        validPoint = false;
+                    }
+                   
+                 for (int i = 0; i < pointInput.length();, i++) {
+                    char a = pointInput.charAt(i);
+                    if (a < '0' || a > '9'
+                        validPoint = false;
+                        break;
+                 }
+                }
+                
+
+
+                
+            }
+
+
+            }
 
 
 
 
 
 
+
+
+
+
+
+
+            
+        }
+
+        
+
+
+      
 
 
 
