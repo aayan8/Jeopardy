51e390b160c203a986ebf91554a66429faf7b5eb
aayan8 <348752791@gapps.yrdsb.ca>
2025-06-09 14:57:32 +0000

    finished step one and 2 and fixed errors

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
+        // Arrays to store up to 100 questions and answers
         String[] categories = new String[100];
         int[] pointAmount = new int[100];
         String[] questionText = new String[100];
         String[] questionAnswer = new String[100];
-        int questionAmmount = 0;
+        int questionAmount = 0;
 
         while (true) {
+            // Main menu display
             System.out.println("Jeopardy Main Menu");
             System.out.println("1. Play Game");
             System.out.println("2. Add Question");
@@ -17,37 +21,72 @@ public class Main {
             System.out.println("Select an option");
             String menuChoice = sc.nextLine();
             
-            if (input.equals("1")) {
+            
+            if (menuChoice.equals("1")) {
                 System.out.println("");
-            } else if (input.equals("2")) {
+            // Add a new question
+            } else if (menuChoice.equals("2")) {
                 System.out.println("Enter category:");
                 categories[questionAmount] = sc.nextLine();
 
+                 // Get and validate point value
                 int points = 0;
                 while (true) {
                     System.out.println("Enter point value (number): ");
-                    String pointInput = sc.nctLine();
-
+                    String pointInput = sc.nextLine();
                     boolean validPoint = true;
-                    if (pointInput.lenght() == 0) {
+
+                    if (pointInput.length() == 0) {
                         validPoint = false;
                     }
                    
-                 for (int i = 0; i < pointInput.length();, i++) {
+                  // Check if input contains only digits
+                 for (int i = 0; i < pointInput.length(); i++) {
                     char a = pointInput.charAt(i);
-                    if (a < '0' || a > '9'
+                    if (a < '0' || a > '9') {
                         validPoint = false;
                         break;
+                    }
                  }
+                
+                
+                if (validPoint) {
+                    points = Integer.parseInt(pointInput);
+                    if (points > 0) {
+                        break;
+                    } else {
+                        System.out.println("Please enter a number greater than 0");
+                    }
+                } else {
+                        System.out.println("Invalid input. Please enter numbers only.");
                 }
-                
-
-
-                
             }
 
+                
+                pointAmount[questionAmount] = points;
 
+                System.out.print("Enter the question text: ");
+                questionText[questionAmount] = sc.nextLine();
+                
+                System.out.print("Enter the correct answer: ");
+                questionAnswer[questionAmount] = sc.nextLine();
+
+                // Increase question count
+                questionAmount++;
+                System.out.println("Question has been added! ");
+                System.out.println("Returning to the main menu");
+            
             }
+
+             else if (menuChoice.equals("3")) {
+                System.out.println("Thanks for playing!");
+                break;
+             } else {
+                System.out.println("Invalid input. Please enter 1, 2, or 3.");
+             }
+          }
+     }
+  }
 
 
 
@@ -61,7 +100,7 @@ public class Main {
 
 
             
-        }
+        
 
         
 
@@ -71,5 +110,4 @@ public class Main {
 
 
         
-    }
-}
+    
